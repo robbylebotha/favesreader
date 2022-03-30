d179fee5e03d4172c40581c3a16157aa13cf684f
Robert M <robbylebotha@gmail.com>
2022-03-30 10:59:17 +0200

    good

--- src/FavouriteColor.java
+++ src/FavouriteColor.java
@@ -4,24 +4,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FavouriteColor {
-    private ArrayList<String> users;
+    private ArrayList<String> users = null;
+    public HashMap<String,String> votedColors;
+    private HashMap<String,String> votedUsers;
+    public String bestColor = null;
+    public ArrayList<String> fullUsernamesList = null;
 
-    public FavouriteColor(){
-        
+    public FavouriteColor(HashMap<String,String> votedColors,
+    HashMap<String,String> votedUsers){
+        this.votedColors = votedColors;
+        this.votedUsers = votedUsers;
+        getFavouriteColor();
+        getFaveColorVoters();
     }
 
     /**
      * Get fave color
+     * @param HashMap<String,String>
      * @return A string representing the most voted for color
      */
-    public String getFavouriteColor(HashMap<String,String> votedColors){
-        // System.out.println("----------START--------------\n\n");
-        int highestNum = 0;
-        String bestColor = null;
-        users = new ArrayList<>();
-        HashMap<String, Integer> freqCount = new HashMap<>();
+    private String getFavouriteColor(){
+        /**
+         * The goal here is to iterate through the votes map and
+         * see which color has been voted the most. We will do this
+         * by creating a new KV map where we insert K=color and V=count.
+         * Every iteration we will check if color exists, if it does we increment
+         * the count, if not we set color and value to 1.
+         */
 
-         // Iterating HashMap through for loop
+        //to hold current highest voted total
+        int highestNum = 0; 
+        //hold users who voted for top color
+        users = new ArrayList<>(); 
+        //this map will hold k=color v=no of time its voted for
+        HashMap<String, Integer> freqCount = new HashMap<>();
+ 
+        //iterate over votes map
          for (Map.Entry<String, String> set :
          votedColors.entrySet()) {
 
@@ -42,10 +60,8 @@ public class FavouriteColor {
 
         }
 
-        // System.out.println(freqCount);
-        // System.out.println("----------END--------------");
-        System.out.println("Best Color: "+bestColor+" votes: "+highestNum);
-        return (bestColor.toUpperCase()+" votes: "+highestNum);
+        bestColor = bestColor.toUpperCase()+" voted by "+highestNum+" users";
+        return (bestColor);
 
     }
 
@@ -53,19 +69,29 @@ public class FavouriteColor {
      * Get a list of those who voted for the top color
      * @return ArrayList of voters
      */
-    public ArrayList<String> getFaveColorVoters(HashMap<String,String> votedUsers){
-        ArrayList<String> fullUsernamesList = new ArrayList<>();
+    private ArrayList<String> getFaveColorVoters(){
+        fullUsernamesList = new ArrayList<>();
 
-        for (String user : users) {
-            if(votedUsers.keySet().contains(user)){
-                fullUsernamesList.add(votedUsers.get(user));
+        try{
+
+            //Compare the userIds of the users who voted for best color
+            //to the user hashmap to get the names of the users
+            //comparable to SQL statement such as
+            //"SELECT users.username FROM users WHERE users.userid = votes.userID"
+            for (String user : users) {
+                if(votedUsers.keySet().contains(user)){
+                    fullUsernamesList.add(votedUsers.get(user));
+                }
             }
+    
+            //sort alphabetically the list of users
+            Collections.sort(fullUsernamesList);
+    
+            return fullUsernamesList;
+        }catch (Exception e){
+            System.out.println(e.toString());
+            return null;
         }
-
-        //sort alphabetically the list of users
-        Collections.sort(fullUsernamesList);
-
-        return fullUsernamesList;
     }
     
 }
